2aae9dc3afff3b5ee4c73386d4471e5e70ff3aa7
Aurelien Balland Chatignon <50362363+daftcloud@users.noreply.github.com>
2026-01-07 11:51:54 +0100

    Nettoyage + Création page print + Modification generate-pdf

--- generate-pdf.js
+++ generate-pdf.js
@@ -2,17 +2,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 
-const pages = [
-  "1_presentation.html",
-  "2_FRBR.html",
-  "3_autorite.html",
-  "4_index.html",
-  "5_remarques.html",
-  "6_conclusion.html",
-  "7_index.html"
-];
-
-const baseURL = "https://daftcloud.github.io/guideline/";
+const url = "https://daftcloud.github.io/guideline/print.html"
 
 const rootDir = process.cwd();
 const outputDir = path.join(rootDir, "assets", "pdf");
@@ -28,28 +18,25 @@ const outputPath = path.join(outputDir, "guideline.pdf");
   });
 
   const page = await browser.newPage();
-  const pdfBuffers = [];
+  
+  await page.goto(url, { waitUntil: "networkidle0" });
 
-  for (const p of pages) {
-    console.log("Processing:", baseURL + p);
-    await page.goto(baseURL + p, { waitUntil: "networkidle0" });
-
-    const pdfBuffer = await page.pdf({
-      format: "A4",
-      printBackground: true,
-      margin: {
-        top: "2cm",
-        bottom: "2cm",
-        left: "2cm",
-        right: "2cm"
-      }
-    });
-
-    pdfBuffers.push(pdfBuffer);
+   const outputDir = path.dirname(outputPath);
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
   }
 
-  const finalBuffer = Buffer.concat(pdfBuffers);
-  fs.writeFileSync(outputPath, finalBuffer);
+  await page.pdf({
+    path: outputPath,
+    format: "A4",
+    printBackground: true,
+    margin: {
+      top: "2cm",
+      bottom: "2cm",
+      left: "2cm",
+      right: "2cm"
+    }
+  });
 
   await browser.close();
   console.log("PDF généré :", outputPath);
---
+++ print.md
@@ -0,0 +1,13 @@
+---
+layout: default
+title: Export PDF
+nav_exclude: true
+---
+
+{% include_relative 1_presentation.md %}
+{% include_relative 2_FRBR.md %}
+{% include_relative 3_autorite.md %}
+{% include_relative 4_index.md %}
+{% include_relative 5_remarques.md %}
+{% include_relative 6_conclusion.md %}
+{% include_relative 7_index.md %}
